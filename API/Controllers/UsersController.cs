ab61ba3c1fd78d90b8638b44b6e0610d5e821b20
Magno <jmagno.dutra@hotmail.com>
2025-01-02 18:53:38 -0300

    Creating AccountController

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,3 @@
-using System;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -6,22 +5,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")] // /api/classname without controller word
-public class UsersController(DataContext context) : ControllerBase
+public class UsersController(DataContext context) : BaseApiController
 {
 
   [HttpGet]
-  public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+  public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+  {
     var users = await context.Users.ToListAsync();
     return users;
   }
 
   [HttpGet("{id:int}")]
-  public async Task<ActionResult<AppUser>> GetUser(int id){
+  public async Task<ActionResult<AppUser>> GetUser(int id)
+  {
     var user = await context.Users.FindAsync(id);
 
-    if(user == null) return NotFound();
+    if (user == null) return NotFound();
 
     return user;
   }
